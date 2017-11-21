a828d5a20247af481768dada431b4168ff8fa86c
mangesh-trimbake <31955330+mangesh-trimbake@users.noreply.github.com>
2017-11-21 11:12:38 +0530

    xlsx to list python code added and some file added

--- decisionTree/csvtolist.py
+++ decisionTree/csvtolist.py
@@ -1,6 +1,24 @@
 import csv
-with open('WA_Fn-UseC_-HR-Employee-Attrition.csv', 'rb') as f:
+with open('Attrition.csv', 'rb') as f:
     reader = csv.reader(f)
     your_list = list(reader)
 
-print your_list
+print len(your_list)
+
+header = your_list[0]
+#print(header,"\n\n")
+
+label = header[1]
+#print(label,"\n\n")
+
+del header [1]
+#print(header,"\n\n")
+
+header.append(label)
+#print(header,"\n\n")
+
+training_data = your_list
+del training_data[0]
+
+print(training_data[0])
+
---
+++ decisionTree/xlsxToList.py
@@ -0,0 +1,12 @@
+from xlrd import open_workbook
+
+file_var = open_workbook("WA_Fn-UseC_-HR-Employee-Attrition.csv.xlsx") 
+
+sheet = file_var.sheet_by_index(0)
+
+list_data = []
+
+for k in range(1,sheet.nrows):
+    list_j.append(str(sheet.row_values(k)[j-1]))
+
+print(len(list_data))
